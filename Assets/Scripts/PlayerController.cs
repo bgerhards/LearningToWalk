e9a76245e6c80bedbcc44dc5dc56e3bc12e6e398
Brian Gerhards <briangerhards@gmail.com>
2020-02-09 20:37:35 -0600

    adds sounds and more

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,19 @@ public class PlayerController : MonoBehaviour
     float rotSpeed = 160;
 
     Rigidbody rb;
+    AudioSource audioSource;
 
     bool isGrounded = false;
     float timeLastJumped = 0;
 
+    [SerializeField]
+    private AudioClip jumpSound;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     void OnCollisionStay(Collision collision)
@@ -51,8 +57,9 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        rb.AddForce(new Vector3(0, 2, 0) * 2, ForceMode.Impulse);
+        audioSource.PlayOneShot(jumpSound);
         isGrounded = false;
+        rb.AddForce(new Vector3(0, 2, 0) * 2, ForceMode.Impulse);
         timeLastJumped = Time.realtimeSinceStartup;
     }
 }
